47f0cb46001fad127299f467e4dd7fa810fca035
unknown <yacoubi.fatima@esprit.tn>
2022-06-26 23:49:53 +0100

    navbar

---
+++ patisserie/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ patisserie/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,24 @@
+export const MenuItems =[
+    {
+        title:'Home',
+        url:'#',
+        cName: 'nav_links'
+    },
+    {
+        title:'Services',
+        url:'#',
+        cName: 'nav_links'
+    },{
+        title:'Products',
+        url:'#',
+        cName: 'nav_links'
+    },{
+        title:'Contact Us',
+        url:'#',
+        cName: 'nav_links'
+    },{
+        title:'Sign Up',
+        url:'#',
+        cName: 'nav_links'
+    }
+]
